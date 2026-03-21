4721ceaebd37049b28baefb8103b251129b5a11c
XH Ding <3187385775@qq.com>
2026-03-21 19:11:20 +0800

    Delete wetland_ticket_app_native directory

--- wetland_ticket_app_native/build_macos_linux.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-cd "$(dirname "$0")"
-python3 -m pip install -r requirements-packaging.txt
-pyinstaller --clean wetland_ticket_desktop.spec
--- wetland_ticket_app_native/build_windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-python -m pip install -r requirements-packaging.txt
-pyinstaller --clean wetland_ticket_desktop.spec
-pause
